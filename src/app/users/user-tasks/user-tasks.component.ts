a81712c5c3bd29b976be14010761f1c4d6e8de8b
Kyle V <kcvaughn75@gmail.com>
2024-10-22 14:41:31 -0400

    initial commit

---
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -0,0 +1,19 @@
+import { Component, computed, inject, input } from '@angular/core';
+import { UsersService } from '../users.service';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-user-tasks',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './user-tasks.component.html',
+  styleUrl: './user-tasks.component.css',
+})
+export class UserTasksComponent {
+  userId = input.required<string>();
+  private usersService = inject(UsersService);
+
+  userName = computed(
+    () => this.usersService.users.find((u) => u.id === this.userId())?.name
+  );
+}
